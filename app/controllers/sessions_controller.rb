820042e77e94b2e9028d88aa5f022482ae9937a6
chihiro7326 <chi_chan1637@yahoo.co.jp>
2019-12-21 06:13:52 +0000

    ログイン

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email_params)
+    if user && user.authenticate(password_params)
       log_in user
       redirect_to root_path, success: "ログインに成功しました"
     else
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:name, :eamil, :password, :password_confirmation)
+    params.require(:user).permit(:name, :eamil, :password)
   end
   
   def log_in(user)
